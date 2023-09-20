41fb3f9f0d701ff526eeb679a459d4f3c730b843
huyJR <111648272+huyJR@users.noreply.github.com>
2023-09-21 01:09:27 +0700

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+t=(1,2, "skip a few", 99, 100)
+
+print(type(t)) #in ra kiểu dữ liệu của t 
+print(t)       #in ra t 
+print(t[4])    # in ra giá trị của phần tử thứ 4  ở vị trí thứ 5 
+
+
+
