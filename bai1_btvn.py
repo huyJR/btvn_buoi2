02502cf30d1f905e75bcd44c8247d2ffdea2acf1
huyJR <111648272+huyJR@users.noreply.github.com>
2023-09-21 00:53:12 +0700

    Create bai1_btvn.py

---
+++ bai1_btvn.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 00:51:26 2023
+
+@author: OS
+"""
+
+t=(1,2, "skip a few", 99, 100)
+
+print(type(t)) #in ra kiểu dữ liệu của t 
+print(t)       #in ra t 
+print(t[4])    # in ra giá trị của phần tử thứ 4  ở vị trí thứ 5 
