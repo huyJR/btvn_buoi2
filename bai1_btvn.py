7aff8683402f2267727038545a8c901fbe531175
huyJR <111648272+huyJR@users.noreply.github.com>
2023-09-21 01:24:12 +0700

    Update bai1_btvn.py

--- bai1_btvn.py
+++ bai1_btvn.py
@@ -4,9 +4,18 @@ Created on Thu Sep 21 00:51:26 2023
 
 @author: OS
 """
-
-t=(1,2, "skip a few", 99, 100)
+# phần 1 kiểu dữ liệu tuples 
+"""t=(1,2, "skip a few", 99, 100)
 
 print(type(t)) #in ra kiểu dữ liệu của t 
 print(t)       #in ra t 
-print(t[4])    # in ra giá trị của phần tử thứ 4  ở vị trí thứ 5 
+print(t[4])    # in ra giá trị của phần tử thứ 4  ở vị trí thứ 5 """
+
+#kiểu dữ liệu list 
+L=[1,2,3,4,5,6,100]
+
+print(" the fisrt item is ", L[0])
+print(" the second item is ", L[1])
+print(" the last item is ", L[-1]) #giá trị tiếp theo của giá trị đầu sẽ đẩy về cuối list 
+print(" the second to last item is ", L[-2])#giá trị tiếp theo của giá trị đầu sẽ đẩy về kế cuối list 
+
